e51974c6f12e7cc7986f8a162ab9091088bad388
Juanma Perales <sayantjm@gmail.com>
2021-02-21 09:46:50 +0100

    Create google proxy Example. Closes #1

---
+++ src/main/java/sayant/springframeworkguru/sfgurubrewerygateway/config/GoogleConfig.java
@@ -0,0 +1,24 @@
+package sayant.springframeworkguru.sfgurubrewerygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Created by sayantjm on 21/2/21
+ */
+@Profile("google")
+@Configuration
+public class GoogleConfig {
+
+    @Bean
+    public RouteLocator googleRouteConfig(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/googlesearch")
+                        .filters(f -> f.rewritePath("/googlesearch(?<segment>/?.*)", "/${segment}"))
+                        .uri("https://google.com"))
+                .build();
+    }
+}
