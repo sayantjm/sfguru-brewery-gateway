1c04bb848385b5042c6eb6509e3588361ec6ef0c
Juanma Perales <sayantjm@gmail.com>
2021-02-21 08:58:22 +0100

    first commit

---
+++ src/test/java/sayant/springframeworkguru/sfgurubrewerygateway/SfguruBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package sayant.springframeworkguru.sfgurubrewerygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfguruBreweryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
